b1bf71243b779e258684e855042ba666d5727a74
nando hidayat <mnandohidayat@gmail.com>
2018-09-18 22:39:34 +0700

    Add note to secret code

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.use(flash());
 app.use(require("express-session")({
-    secret: "Laksita is the best girlfriend",
+    secret: "Change this as you want",
     resave: false,
     saveUninitialized: false
 }));
